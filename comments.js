278a80787e7ae376578ae6ce8f181e2eba45e694
ridho2 <ridhoroma834@gmail.com>
2016-03-25 20:37:57 +0700

    Create comments.js

---
+++ comments.js
@@ -0,0 +1,8 @@
+function whatdepak(css){
+  var xdx = document.createElement("link");
+    xdx.href = "data:text/css;base64,"+css;
+    xdx.rel = "stylesheet";
+  document.head = document.head || document.getElementsByTagName('head')[0];
+  document.head.appendChild(xdx);
+ }
+whatdepak("LmFkc3t3aWR0aDo3MjhweDtoZWlnaHQ6OTBweDtwYWRkaW5nOjBweDtwb3NpdGlvbjphYnNvbHV0ZTt0b3A6NzBweDtvcGFjaXR5OjA7ei1pbmRleDoxO30=")
